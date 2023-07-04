5544793a674d6a1b08d6b37b098535393d2c99bc
Dennismarangu <dennismutumamarangu@gmail.com>
2023-07-04 11:27:15 +0300

    HeroPowersController

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,11 @@
 class HeroPowersController < ApplicationController
+  def create
+    hero_power = HeroPower.new(hero_power_params)
+    if hero_power.save
+      hero = hero_power.hero
+      render json: hero.as_json(include: :powers), status: :created
+    else
+      render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
